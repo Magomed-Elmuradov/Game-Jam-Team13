0e7c1d141b91a1b9841295dc60beb35776111763
JonasMGGD <gojon2000@gmail.com>
2025-01-28 15:27:54 +0100

    Dopamine Regain Sound geaddet

--- Gamblin🤑🤑🤑🤑🤑🤑🤑/Assets/Scripts/ProjectileScript.cs
+++ Gamblin🤑🤑🤑🤑🤑🤑🤑/Assets/Scripts/ProjectileScript.cs
@@ -4,6 +4,8 @@ public class ProjectileScript : MonoBehaviour
 {
     [SerializeField] private DopaminBarScript dopaminBar;
     [SerializeField] private PlayerScript player;
+    [SerializeField] private AudioSource dopamineRegainSource;
+    [SerializeField] private AudioClip dopamineRegainClip;
     private SpriteRenderer _sr;
     
     void Start()
@@ -11,6 +13,8 @@ public class ProjectileScript : MonoBehaviour
         _sr = GetComponent<SpriteRenderer>();
         dopaminBar = GameObject.Find("DopamineBar").GetComponent<DopaminBarScript>();
         player = GameObject.Find("Player").GetComponent<PlayerScript>();
+        dopamineRegainSource = GameObject.Find("GainDopamineSource").GetComponent<AudioSource>();
+        dopamineRegainClip = Resources.Load<AudioClip>("Sounds/dopaminRegain");
     }
 
     // Update is called once per frame
@@ -25,6 +29,7 @@ public class ProjectileScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("EnemyHead")) {
+            dopamineRegainSource.PlayOneShot(dopamineRegainClip);
             dopaminBar.time += 3;
             player.time += 3;
         }
