9ce2b2f64e4df3f9dcc4aa25dfd6de4859828d72
JonasMGGD <gojon2000@gmail.com>
2025-01-30 09:17:00 +0100

    Merge Fix

---
+++ Gamblin🤑🤑🤑🤑🤑🤑🤑/Assets/Scripts/GameScene/DiceScript.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+namespace GameScene {
+    public class DiceScript : MonoBehaviour {
+        private static readonly int Dead = Animator.StringToHash("Dead");
+        private static readonly int Grounded = Animator.StringToHash("Grounded");
+        private static readonly int Jumping = Animator.StringToHash("Jumping");
+        [SerializeField] private Animator animator;
+        [SerializeField] private DopaminBarScript dopaminBar;
+        [SerializeField] private float speed;
+
+        public Rigidbody2D rb;
+        private SpriteRenderer _sr;
+        internal bool _grounded;
+        internal float _waitTime = 1f;
+
+        public bool alive = true;
+
+        void Start() {
+            rb = GetComponent<Rigidbody2D>();
+            _sr = GetComponent<SpriteRenderer>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (alive == false) {
+                animator.SetBool(Dead, true);
+                foreach (Transform child in transform) {
+                    Destroy(child.gameObject);
+                }
+            }
+
+            if (_grounded && alive && GetComponent<SpriteRenderer>().isVisible) {
+                _waitTime -= Time.deltaTime;
+            }
+
+            if (_waitTime <= 0 && alive) {
+                _waitTime = 1f;
+                JumpLeft();
+            }
+        }
+
+        internal void JumpLeft() {
+            rb.linearVelocity = Vector2.zero;
+            rb.AddForce(new Vector2(-5f * speed, 3f * speed), ForceMode2D.Impulse);
+            _grounded = false;
+            animator.SetBool(Grounded, false);
+            animator.SetBool(Jumping, true);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other) {
+            animator.SetBool(Grounded, true);
+            animator.SetBool(Jumping, false);
+            if (other.gameObject.CompareTag("Ground")) {
+                _grounded = true;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.gameObject.CompareTag("Projectile")) {
+                ProjectileScript projectile = other.gameObject.GetComponent<ProjectileScript>();
+                projectile.Shatter();
+                alive = false;
+            }
+        }
+
+        public void AnimationComplete() {
+            Destroy(gameObject);
+        }
+    }
+}
